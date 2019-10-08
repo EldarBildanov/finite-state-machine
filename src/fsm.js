79de98d0ef096845bf50694a157dd321ce7f1e94
EldarBildanov <eldar.bildanov@gmail.com>
2019-10-08 11:48:58 +0300

    feat: add task

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,52 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        if (!config) throw new Error
+        this.state = config.states
+        this.initialState = config.initial
+        this.history = []
+        this.history.push(this.initialState)
+        this.index = 0
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.history[this.index]
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if (!this.state[state]) throw new Error
+
+        if (this.history[this.index + 1]) this.history.pop()
+
+        this.history.push(state)
+        this.index += 1
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        let next = this.state[this.getState()].transitions[event]
+        if (!next) throw new Error
+        this.changeState(next)
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.changeState(this.initialState)
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +56,49 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        let states = []
+        if (event) {
+            for (let i in this.state) {
+                if (this.state[i].transitions[event]) states.push(i)
+            }
+        }
+        else {
+            for (let i in this.state) states.push(i)
+        }
+        return states
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        if (this.index - 1 < 0) return false
+        this.index -=1
+        return true
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        if (this.index + 1 > this.history.length - 1) return false;
+        this.index +=1
+        return true
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.history = []
+        this.history.push(this.initialState)
+        this.index = 0
+    }
 }
 
 module.exports = FSM;
